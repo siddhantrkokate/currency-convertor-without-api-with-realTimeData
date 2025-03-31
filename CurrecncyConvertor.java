b6619eff73605d1ec6764a7a4357147116c25b80
Siddhant <siddhantrkokate@gmail.com>
2025-03-31 20:41:31 +0530

    Project is completed

--- CurrecncyConvertor.java
+++ CurrecncyConvertor.java
@@ -7,7 +7,7 @@ import org.jsoup.nodes.Element;
 import org.jsoup.select.Elements;
 
 // input
-protected class InputHandlers{
+class InputHandlers{
 
     // Array of currency codes which is used forvalidating inputed code
     String currencyCode[] = {"AED", "AFN", "ALL", "AMD", "ANG", "AOA", "ARS", "AUD", "AWG", "AZN","BAM", "BBD", "BDT", "BGN", "BHD", "BIF", "BMD", "BND", "BOB", "BRL","BSD", "BTN", "BWP", "BYN", "BZD", "CAD", "CDF", "CHF", "CLP", "CNY","COP", "CRC", "CUP", "CVE", "CZK", "DJF", "DKK", "DOP", "DZD", "EGP","ERN", "ETB", "EUR", "FJD", "FKP", "FOK", "GBP", "GEL", "GGP", "GHS","GIP", "GMD", "GNF", "GTQ", "GYD", "HKD", "HNL", "HRK", "HTG", "HUF","IDR", "ILS", "IMP", "INR", "IQD", "IRR", "ISK", "JEP", "JMD", "JOD","JPY", "KES", "KGS", "KHR", "KID", "KMF", "KRW", "KWD", "KYD", "KZT","LAK", "LBP", "LKR", "LRD", "LSL", "LYD", "MAD", "MDL", "MGA", "MKD", "MMK", "MNT", "MOP", "MRU", "MUR", "MVR", "MWK", "MXN", "MYR", "MZN", "NAD", "NGN", "NIO", "NOK", "NPR", "NZD", "OMR", "PAB", "PEN", "PGK","PHP", "PKR", "PLN", "PYG", "QAR", "RON", "RSD", "RUB", "RWF", "SAR","SBD", "SCR", "SDG", "SEK", "SGD", "SHP", "SLE", "SOS", "SRD", "SSP","STN", "SYP", "SZL", "THB", "TJS", "TMT", "TND", "TOP", "TRY", "TTD", "TVD", "TWD", "TZS", "UAH", "UGX", "USD", "UYU", "UZS", "VES", "VND","VUV", "WST", "XAF", "XCD", "XDR", "XOF", "XPF", "YER", "ZAR", "ZMW", "ZWL"};
@@ -60,7 +60,7 @@ protected class InputHandlers{
  
 }
 
-protected class RealJsoup extends InputHandlers{
+class RealJsoup extends InputHandlers{
 
 
     // formatting url
@@ -126,11 +126,11 @@ public class CurrecncyConvertor extends RealJsoup{
     // function: for compile time input
     double convertCurrency(String convertFrom, String convertTo){
         if(!validate(convertFrom)){
-            System.out.println("InputExceptionOccured: Wrong Currency Code Inputed!")
+            System.out.println("InputExceptionOccured: Wrong Currency Code Inputed!");
             return 0.0;
         }
         if(!validate(convertTo)){
-            System.out.println("InputExceptionOccured: Wrong Currency Code Inputed!")
+            System.out.println("InputExceptionOccured: Wrong Currency Code Inputed!");
             return 0.0;
         }
         return currencyCovertor(convertFrom,convertTo);
